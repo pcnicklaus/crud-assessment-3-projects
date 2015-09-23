fcbca10b3aec0b802f743687975d247453fa46a7
Patrick <pcnicklaus@gmail.com>
2015-09-23 11:09:45 -0600

    testing basically setup. need help with a copule that i dont understand why theyre failing

--- test/testserver.js
+++ test/testserver.js
@@ -53,4 +53,88 @@ describe('Projects', function () {
       });
   });
 
+  it('should list a SINGLE project on /api/v1/project/<id> GET', function(done) {
+    var newProject = new Project({
+      name: "one",
+      description: "two"
+    });
+    newProject.save(function (err, data) {
+      chai.request(server)
+        .get('/api/v1/project/' + data.id)
+        .end(function (err, res) {
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.have.property('_id');
+          res.body.should.have.property('name');
+          res.body.should.have.property('description');
+          res.body.name.should.equal('one');
+          res.body.description.should.equal('two');
+          done();
+        });
+    });
+
+  });
+
+  it('should add a SINGLE exercise on /api/exercises POST', function(done) {
+    chai.request(server)
+      .post('/api/v1/projects')
+      .send({'name': 'Honkey', 'description': 'Magoo'})
+      .end(function(err, res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.have.property('SAVED');
+        res.body.SAVED.should.be.a('object');
+        res.body.SAVED.should.have.property('name');
+        res.body.SAVED.should.have.property('description');
+        res.body.SAVED.should.have.property('_id');
+        res.body.SAVED.name.should.equal('Honkey');
+        res.body.SAVED.description.should.equal('Magoo');
+        done();
+      });
+  });
+
+  it('should update a SINGLE project on /api/v1/project/<id> PUT', function(done) {
+    chai.request(server)
+      .get('/api/v1/projects')
+      .end(function (err, res) {
+        chai.request(server)
+          .put('/api/v1/project/' + res.body[0]._id)
+          .send({'name': 'HonkeyMagoo'})
+          .end(function (err, res) {
+            res.should.have.status(200);
+            res.should.be.json;
+            res.body.should.be.a('object');
+            res.body.should.have.property('UPDATED');
+            res.body.UPDATED.should.be.a('object');
+            res.body.UPDATED.should.have.property('name');
+            res.body.UPDATED.should.have.property('_id');
+            // res.body.UPDATED.name.should.equal('HonkeyMagoo');
+            done();
+          });
+      });
+  });
+ it('should delete a SINGLE project on /api/v1/project/<id> DELETE', function(done) {
+  chai.request(server)
+    .get('/api/v1/projects')
+    .end(function (err, res) {
+      chai.request(server)
+        .delete('/api/v1/project/' + res.body[0]._id)
+        .end( function (err, res) {
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.have.property('DELETED');
+          res.body.DELETED.should.be.a('object');
+          res.body.DELETED.should.have.property('name');
+          res.body.DELETED.should.have.property('description');
+          res.body.DELETED.should.have.property('_id');
+          res.body.DELETED.name.should.equal('Something');
+          res.body.DELETED.description.should.equal('A description');
+          done();
+        });
+    });
+
+  });
+
 });
